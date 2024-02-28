86fd9683e81462da368db2fb977a3b4733a8a752
maikol oropeza <maikol.oropeza@bplay.com.ar>
2024-02-28 11:55:53 -0300

    Five Commit

--- script/scriptpage1.js
+++ script/scriptpage1.js
@@ -93,7 +93,6 @@ function buttonComenzar() {
   let participantsValue = document.querySelector("#participants").value;
   // aca se divide el contenido en lineas
   let lines = participantsValue.split("\n");
-  console.log(lines);
   // se filtran las líneas que no están en blanco
   let nonEmptyLines = lines.filter(function (line) {
     // elimina espacios en blanco al principio y al final de la línea
@@ -108,71 +107,104 @@ function buttonComenzar() {
   if (lines.length > 1 && tituloSorteo.length > 0) {
     window.location.href = "./pages/parameters.html"; // Cambia la ruta según sea necesario
   } else {
-    alert("- Título y/o Participantes no pueden estar vacíos\n- Debe haber más de un participante. ")
-    
+    alert(
+      "- Título y/o Participantes no pueden estar vacíos\n- Debe haber más de un (1) participante. "
+    );
+  }
 }
-}
-//Rescatar todos los parametros en el localStorage
 function onloadParameters() {
   // rescata el valor del nombre del titulo y lo agrega el contenido a la pagina
-  document.querySelector(".tituloSorteo").textContent = localStorage.getItem("tituloSorteo");
-  
+  document.querySelector(".tituloSorteo").textContent =
+    localStorage.getItem("tituloSorteo");
+
   // Rescatar los participantes del localStorage
   let storageParticipants = localStorage.getItem("participantes").split("\n");
-  document.querySelector(".totalP span").textContent = storageParticipants.length;
-  
+  document.querySelector(".totalP span").textContent =
+    storageParticipants.length;
+
   //rescatar los valores de los parametros
   let cantidadGanadores = document.querySelector("#winnersnmber");
   let cantidadSuplentes = document.querySelector("#subwinnersNmber");
   let filtrarDuplicados = document.querySelector("#toggle");
   let chancesExtra = document.querySelector("#toggle2");
   let parametro = document.querySelector("#parameTexarea");
-  
-  // Almacenamos datos en localStorage
-  localStorage.setItem("cantidadGanadores", cantidadGanadores.value);
-  localStorage.setItem("cantidadSuplentes", cantidadSuplentes.value);
-  localStorage.setItem("filtrarDuplicados", filtrarDuplicados.value);
-  localStorage.setItem("chancesExtra", chancesExtra.value);
-  localStorage.setItem("parametro", parametro.value);
 
-  // Sortear
+  // BUTTON SORTEAR
   document.querySelector("#buttonSorteo").addEventListener("click", () => {
+    if(filtrarDuplicados.value === '1'){
+        const listaSet = new Set(storageParticipants)
+      storageParticipants = [...listaSet]
+    }
+
+    let ganadores = [];
+    let suplentes = [];
     // elegir el ganador usando la libreria Math
-    const ganador =
-      storageParticipants[
-        Math.floor(Math.random() * storageParticipants.length)
-      ];
-    // elegir el suplente
-    let suplente =
-      storageParticipants[
-        Math.floor(Math.random() * storageParticipants.length)
-      ];
-    // condicionar para que el suplente no sea igual al ganador
-    while (suplente === ganador) {
-      suplente =
+    for (let i = 0; i < Number(cantidadGanadores.value); i++) {
+      const ganador =
         storageParticipants[
           Math.floor(Math.random() * storageParticipants.length)
         ];
+      ganadores.push(ganador);
+      const listaParticipantes = storageParticipants.filter(
+        (item) => item !== ganador
+      );
+      localStorage.setItem("participantes", listaParticipantes);
     }
+    // BUSCAR LOS SUPLENTES
+    if (Number(cantidadSuplentes.value) !== 0) {
+      for (let i = 0; i < Number(cantidadSuplentes.value); i++) {
+        let suplente =
+        storageParticipants[
+          Math.floor(Math.random() * storageParticipants.length)
+        ];
+        const listaParticipantes = storageParticipants.filter(
+          (item) => item !== suplente
+        );
+        localStorage.setItem("participantes", listaParticipantes);
+        suplentes.push(suplente)
+      }
+      document.querySelector('#contenedorSuplente').style.display = 'block'
+
+       // DOM DE SUPLENTES 
+      const suplenteDisplay = document.querySelector("#suplente")
+      suplentes.map(suplente => {
+        const contenidoSuplente = document.createElement('p')
+        contenidoSuplente.classList.add('box')
+        contenidoSuplente.textContent = suplente
+        suplenteDisplay.appendChild(contenidoSuplente)
+      })
+    }
+
     // Llamar cuenta regresiva
     cronometro();
 
-    document.querySelector("#ganador").textContent = ganador;
-    document.querySelector("#suplente").textContent = suplente;
+    // DOM DE GANADOR 
+    const ganadorDisplay = document.querySelector("#ganador")
+    ganadores.map(ganador => {
+      const contenidoGanador = document.createElement('p')
+      contenidoGanador.classList.add('box')
+      contenidoGanador.textContent = ganador
+      ganadorDisplay.appendChild(contenidoGanador)
+    })
+
+    // CONDICIONAR EL TITULO DE GANADOR Y SUPLENTES SI ES PLURAL O NO
+    document.querySelector('#tituloSuplente').textContent = Number(cantidadSuplentes.value) > 1 ? "Suplentes:" : "Suplente:"
+    document.querySelector('#tituloGanador').textContent = Number(cantidadGanadores.value) > 1 ? "Ganadores:" : "Ganador:"
   });
+  
 
   // Hacer la cuenta regresiva y cambiar el display para ver el conteo y luego el ganador
   function cronometro() {
     document.querySelector(".containerParameters").style.display = "none";
-    document.querySelector("#sorteando").style.display = "block";
+    document.querySelector("#sorteando").style.display = "initial";
     let segundos = 3;
-    
+
     function actualizarSegundo() {
       cuentaRegresiva(segundos);
-      
+
       if (segundos === 0) {
         document.querySelector("#sorteando").style.display = "none";
-        document.querySelector("#resultado").style.display = "initial";
+        document.querySelector("#resultado").style.display = "flex";
         document.querySelector("#confeti").style.display = "initial";
       } else {
         segundos--;
@@ -181,92 +213,104 @@ function onloadParameters() {
     }
     actualizarSegundo();
   }
-  
+
   // Mostrar los segundos cada vez que pasa un segundo
   function cuentaRegresiva(segundos) {
     let cuentaRegresiva = document.querySelector("#sorteando h2");
     cuentaRegresiva.textContent = segundos;
   }
- 
+
   //Limpiar todos los valores del localStorage cuando va al home despues de terminar todo elsorteo
   document.querySelector("#resetSorteo").addEventListener("click", () => {
     localStorage.clear();
   });
 
   // aumentar valor de ganadores
-  document.querySelector('.botonUp1').addEventListener('click', ()=>{
-    
-    if(Number(cantidadGanadores.value) > 0 && Number(cantidadGanadores.value) < 10){
-      const NuevaCantidadCanadores = Number(cantidadGanadores.value) + 1
-      cantidadGanadores.value = NuevaCantidadCanadores
-      console.log(cantidadGanadores.value);
-      localStorage.setItem("cantidadGanadores", NuevaCantidadCanadores);
+  document.querySelector(".botonUp1").addEventListener("click", () => {
+    if (
+      Number(cantidadGanadores.value) > 0 &&
+      Number(cantidadGanadores.value) < 10 &&
+      Number(cantidadGanadores.value) < (storageParticipants.length - 1)
+    ) {
+      cantidadGanadores.value = Number(cantidadGanadores.value) + 1;
     }
-  })
+  });
   // disminuir valor de ganadores
-  document.querySelector('.botonDown1').addEventListener('click', ()=>{
-
-    if(Number(cantidadGanadores.value) > 1 && Number(cantidadGanadores.value) <= 10 && Number(cantidadSuplentes.value) < (Number(cantidadGanadores.value) - 1)){
-      const NuevaCantidadCanadores = Number(cantidadGanadores.value) - 1
-      cantidadGanadores.value = NuevaCantidadCanadores
-      localStorage.setItem("cantidadGanadores", NuevaCantidadCanadores);
+  document.querySelector(".botonDown1").addEventListener("click", () => {
+    if (
+      Number(cantidadGanadores.value) > 1 &&
+      Number(cantidadGanadores.value) <= 10 &&
+      Number(cantidadSuplentes.value) < Number(cantidadGanadores.value) - 1
+    ) {
+      cantidadGanadores.value = Number(cantidadGanadores.value) - 1;
     }
-  })
+  });
   //aumentar valor de suplentes
-  document.querySelector('.botonUp2').addEventListener('click', ()=>{
-    if(Number(cantidadGanadores.value) > 1 && Number(cantidadSuplentes.value) < (Number(cantidadGanadores.value) - 1)){
-      const NuevaCantidadSuplentes = Number(cantidadSuplentes.value) + 1
-      cantidadSuplentes.value = NuevaCantidadSuplentes
-      localStorage.setItem("cantidadSuplentes", NuevaCantidadSuplentes);
+  document.querySelector(".botonUp2").addEventListener("click", () => {
+    if (
+      Number(cantidadGanadores.value) > 1 &&
+      Number(cantidadSuplentes.value) < Number(cantidadGanadores.value) - 1 &&
+      Number(cantidadSuplentes.value) + Number(cantidadGanadores.value) <
+        storageParticipants.length
+    ) {
+      cantidadSuplentes.value = Number(cantidadSuplentes.value) + 1;
     }
-  })
+  });
   //disminuir valor de suplentes
-  document.querySelector('.botonDown2').addEventListener('click', ()=>{
-    if(Number(cantidadSuplentes.value) > 0 && Number(cantidadGanadores.value) <= 10){
-      const NuevaCantidadSuplentes = Number(cantidadSuplentes.value) - 1
-      cantidadSuplentes.value = NuevaCantidadSuplentes
-      localStorage.setItem("cantidadSuplentes", NuevaCantidadSuplentes);
+  document.querySelector(".botonDown2").addEventListener("click", () => {
+    if (
+      Number(cantidadSuplentes.value) > 0 &&
+      Number(cantidadGanadores.value) <= 10
+    ) {
+      cantidadSuplentes.value = Number(cantidadSuplentes.value) - 1;
     }
-  })
+  });
 
   // activar y desactivar filtro de duplicado
-  document.querySelector('#toggle').addEventListener('click', ()=>{
-    if(localStorage.getItem("filtrarDuplicados") === "0"){
-      localStorage.setItem("filtrarDuplicados", '1');
-    }else{
-      localStorage.setItem("filtrarDuplicados", '0');
+  document.querySelector("#toggle").addEventListener("click", () => {
+    if (filtrarDuplicados.value === "0") {
+      filtrarDuplicados.value = "1";
+    } else {
+      filtrarDuplicados.value = "0";
     }
-  })
-  
+  });
+
   //activar y descativar filtro chances extra, y activar y desactivar parametro
-  document.querySelector('#toggle2').addEventListener('click', ()=>{
-    if(localStorage.getItem("chancesExtra") === "0"){
-      localStorage.setItem("chancesExtra", '1');
-      parametro.disabled = false
-      parametro.focus()
-    }else{
-      localStorage.setItem("chancesExtra", '0');
-      document.querySelector('#parameTexarea').disabled = true
+  document.querySelector("#toggle2").addEventListener("click", () => {
+    if (chancesExtra.value === "0") {
+      chancesExtra.value = "1";
+      parametro.disabled = false;
+      parametro.focus();
+    } else {
+      chancesExtra.value = "0";
+      document.querySelector("#parameTexarea").disabled = true;
     }
-  })
+  });
   // ir guardando en el localStorage el valor del parametro mediante evento del input
-  parametro.addEventListener('input',()=>{
-    localStorage.setItem('parametro',parametro.value)
-  })
+  parametro.addEventListener("input", () => {
+    localStorage.setItem("parametro", parametro.value);
+  });
+
+  
 }
 
 
-//Parametros de ganador y suplentes. Si ganador es menor a 2 no se puede aumentar el suplente (FINALIZADO)
-
-//Si parametro chances extra es true se activa parametro sino no (FINALIZADO)
-
-//Que aparezca los resultados arriba del confeti (FINALIZADO)
-
-//Aumentar los numeros en los parametros de ganador y suplente (FINALIZADO)
 
 
-// FALTA POR HACER
-// SPINNER - Listo
-// FILTRE DUPLICADOS (FUNCIONABILIDAD)
-// CHANCES EXTRA (FUNCIONABILIDAD)
-//FUNCIÓN DE ELEGIR LA CANTIDAD DE GANADORES/SUPLENTES SELECCIONADOS
+
+
+
+
+
+
+
+
+
+
+// FILTRE DUPLICADOS (FUNCIONABILIDAD) (FINALIZADO)
+
+// CHANCES EXTRA (FUNCIONABILIDAD) (CUANDO SEA NECESARIO)
+
+// --- FALTA POR HACER ---
+
+// HACER EL DOM DE LOS GANADORES Y SUPLENETES (SI HAY SUPLENTES) 
